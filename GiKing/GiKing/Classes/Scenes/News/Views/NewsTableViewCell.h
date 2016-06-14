0ed8d7fa3f28cafe8dcff2cc897ab2e80a5c3d65
LIYALuna <liya1452797429@liyadeMacBook-Pro.local>
2016-06-14 14:33:47 +0800

    我改了Views 文件夹里的内容 我在 BarViewController 里 导入了NewsTableViewsController.m ,并且修改了 NewsViewController 的初始化

---
+++ GiKing/GiKing/Classes/Scenes/News/Views/NewsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewCell.h
+//  GiKing
+//
+//  Created by 李雅 on 16/6/14.
+//  Copyright © 2016年 Wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewCell : UITableViewCell
+
+@end
