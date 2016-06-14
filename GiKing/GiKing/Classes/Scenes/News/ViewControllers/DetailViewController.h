6272361ae516444f2f9eb50ee1643414b96b29ce
LIYALuna <liya1452797429@liyadeMacBook-Pro.local>
2016-06-14 11:26:43 +0800

    我在news文件夹的ViewController文件里添加了DetailViewController文件

---
+++ GiKing/GiKing/Classes/Scenes/News/ViewControllers/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  GiKing
+//
+//  Created by 李雅 on 16/6/13.
+//  Copyright © 2016年 Wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
