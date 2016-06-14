c6228aa22f8b4c2820de3f79161ad5dbb410a301
LIYALuna <liya1452797429@liyadeMacBook-Pro.local>
2016-06-14 11:41:55 +0800

    我添加DetailViewController NewsTableViewCell NewsModel 并且添加了健身素材图片文件夹 我在BarViewController里导入了NewsTableViewController.m，并且把NewsViewController的初始化改成了NewsTableViewController

---
+++ GiKing/GiKing/Classes/Scenes/News/ViewControllers/NewsModel.m
@@ -0,0 +1,13 @@
+//
+//  NewsModel.m
+//  GiKing
+//
+//  Created by 李雅 on 16/6/14.
+//  Copyright © 2016年 Wj. All rights reserved.
+//
+
+#import "NewsModel.h"
+
+@implementation NewsModel
+
+@end
---
+++ GiKing/GiKing/Classes/Scenes/News/ViewControllers/NewsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.m
+//  志同道合
+//
+//  Created by 李雅 on 16/6/12.
+//  Copyright © 2016年 羊羊羊～咩～. All rights reserved.
+//
+
+#import "NewsTableViewCell.h"
+
+@implementation NewsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
