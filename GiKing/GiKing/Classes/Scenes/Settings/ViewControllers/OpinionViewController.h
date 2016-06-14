80d9f83d55a276d1aba30b1cbe2abb7f43398141
KingGi <Han-byeol@RemdeMacBook-Pro.local>
2016-06-14 20:57:24 +0800

    王俊 写了 建议意见里面的发送邮件  背景颜色 没有完成

---
+++ GiKing/GiKing/Classes/Scenes/Settings/ViewControllers/OpinionViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpinionViewController.h
+//  GiKing
+//
+//  Created by Han-byeol on 16/6/14.
+//  Copyright © 2016年 Wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpinionViewController : UIViewController
+
+@end
